4a2bb224b66bee4a04819037558ae2bd775744a4
DayleChettiar <dayle.chettiar@ryerson.ca>
2022-06-04 10:43:22 -0400

    configuring persistence and controllers

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -1,9 +1,15 @@
 package com.globomatics.bike.models;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class Bike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String email;
     private String phone;
@@ -12,6 +18,14 @@ public class Bike {
     private BigDecimal purchasePrice;
     private Date purchaseDate;
     private boolean contact;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     // Adding accessors and mutators
     public String getName() {
